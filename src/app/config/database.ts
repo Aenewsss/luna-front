ef3b426a145c97ead91e774db8d3573cb1ac4312
Aenewsss <aenamartinelli@gmail.com>
2024-10-20 21:39:22 -0300

    first commit

--- src/app/config/database.ts
+++ src/app/config/database.ts
@@ -1,9 +1,8 @@
 import mysql from 'mysql2/promise';
 
 export const db = mysql.createPool({
-  host: 'localhost', // Your MySQL host
-  user: 'root', // Your MySQL user
-  password: 'root', // Your MySQL password
-  database: 'luna', // Your database name
-  port: 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
